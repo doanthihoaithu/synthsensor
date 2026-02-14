5f34904df353f99a8bc5d0ecf29b141b261e9161
macbook <hoaithuxt1998@gmail.com>
2026-02-14 21:54:42 +0100

    add slurm script

---
+++ Python/slurm_run_generation.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=generate_synthetic_data.py
+#SBATCH --nodes=1 # 1 node
+#SBATCH --ntasks-per-node=16 # 32 tasks per node
+#SBATCH --time=24:00:00 # time limits: 1 hour
+#SBATCH --error=slurm_logs/generate_synthetic_data.err # standard error file
+#SBATCH --output=slurm_logs/generate_synthetic_data.out # standard output file
+#SBATCH --partition=gprod_gssi # partition name
+#SBATCH --mail-type=END              # type of event notification
+#SBATCH --mail-user=thihoaithu.doan@gssi.it   # mail address
+
+module load python
+source /home/doan/projects/ibm_scripts/torch210/bin/activate
+python main_new.py
+deactivate
