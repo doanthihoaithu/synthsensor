9fffc256b045c3d330311059507432dde9445112
macbook <hoaithuxt1998@gmail.com>
2026-02-24 14:25:44 +0100

    update slurm script

--- Python/slurm_run_generation.sh
+++ Python/slurm_run_generation.sh
@@ -5,8 +5,7 @@
 #SBATCH --time=24:00:00 # time limits: 1 hour
 #SBATCH --error=slurm_logs/generate_synthetic_data.err # standard error file
 #SBATCH --output=slurm_logs/generate_synthetic_data.out # standard output file
-#SBATCH --partition=gprod_gssi # partition name
-#SBATCH --gres=gpu:a100:1
+#SBATCH --partition=lprod # partition name
 #SBATCH --mail-type=END              # type of event notification
 #SBATCH --mail-user=thihoaithu.doan@gssi.it   # mail address
 
